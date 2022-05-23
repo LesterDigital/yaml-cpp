4a0563b342c71d4e3252f327269a020b6c6f8450
Ilkin Mammadli <lester54539@gmail.com>
2022-05-23 12:31:06 +0400

    Added premake file.

---
+++ premake5.lua
@@ -0,0 +1,31 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+	targetdir ("Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir ("Binaries-Int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++20"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
